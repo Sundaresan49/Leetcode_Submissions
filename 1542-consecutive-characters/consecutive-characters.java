cf9f129651f554b9611c07ac3935613cfd8b5e6a
Sundaresan <abishekrode4@gmail.com>
2026-07-15 20:31:03 +0530

    Time: 1 ms (100.00%) | Memory: 42.6 MB (99.90%) - LeetSync

---
+++ 1542-consecutive-characters/consecutive-characters.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int maxPower(String s) {
+      int max=1;
+      int cnt=1;
+      for(int i=1;i<s.length();i++){
+        if(s.charAt(i)==s.charAt(i-1)){
+            cnt++;
+            max=Math.max(max,cnt);
+            
+        }
+        else{
+            cnt=1;
+        }
+
+      }
+      return max;
+        
+    }
+}
